8ca1a2a01cdcd60c50fc8a5d552f8694bd045a0d
Gabriel <gabriel@basesistemas.com.br>
2019-06-05 08:28:15 -0300

    Finalizando funcionalidade do shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule} from "@angular/forms";
+import {IonicModule} from "@ionic/angular";
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    IonicModule
   ]
 })
 export class SharedModule { }
