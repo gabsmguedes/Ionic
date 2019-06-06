cdcc74be31bf6e673e6c676212dd3f52805202be
Gabriel <gabriel@basesistemas.com.br>
2019-06-06 09:09:25 -0300

    Criando setItem, getAll, Create, Update e Delete

--- src/app/core/classes/firestore.class.ts
+++ src/app/core/classes/firestore.class.ts
@@ -1,7 +1,7 @@
 import {AngularFirestore, AngularFirestoreCollection, QueryFn} from "@angular/fire/firestore";
 import {Observable} from "rxjs";
 
-export abstract class Firestore<T> {
+export abstract class Firestore<T extends { id: string}> {
 
     protected collection: AngularFirestoreCollection<T>;
 
@@ -12,7 +12,27 @@ export abstract class Firestore<T> {
         this.collection = path ? this.db.collection(path, queryFn) : null;
     }
 
+    private setItem(item: T, operation: string): Promise<T>{
+        return this.collection
+            .doc<T>(item.id)
+            [operation](item)
+            .then(() => item);
+    }
+
     getAll(): Observable<T[]> {
         return this.collection.valueChanges();
+    }
+
+    create(item: T): Promise<T> {
+        item.id = this.db.createId();
+        return this.setItem(item, 'set');
+    }
+
+    update(item: T): Promise<T> {
+        return this.setItem(item, 'update');
+    }
+
+    delete(item: T): Promise<void> {
+        return this.collection.doc<T>(item.id).delete();
     }
 }
