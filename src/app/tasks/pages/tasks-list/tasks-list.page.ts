9c28f25ea6290021dbcb1fdf3f51dba0a540c11f
Gabriel <gabriel@basesistemas.com.br>
2019-06-06 13:19:20 -0300

    Deletando tasks

--- src/app/tasks/pages/tasks-list/tasks-list.page.ts
+++ src/app/tasks/pages/tasks-list/tasks-list.page.ts
@@ -1,26 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable, of} from "rxjs";
 import {Task} from "../../models/task.model";
 import {TasksService} from "../../services/tasks.service";
 import {NavController} from "@ionic/angular";
+import {OverlayService} from "../../../core/services/overlay.service";
 
 @Component({
-  selector: 'app-tasks-list',
-  templateUrl: './tasks-list.page.html',
-  styleUrls: ['./tasks-list.page.scss'],
+    selector: 'app-tasks-list',
+    templateUrl: './tasks-list.page.html',
+    styleUrls: ['./tasks-list.page.scss'],
 })
 export class TasksListPage implements OnInit {
 
-  tasks$: Observable<Task[]>;
+    tasks$: Observable<Task[]>;
 
-  constructor(private navController: NavController, private taskService: TasksService) { }
+    constructor(private navController: NavController, private overlayService: OverlayService, private taskService: TasksService) {
+    }
 
-  ngOnInit() {
-    this.tasks$ = this.taskService.getAll();
-  }
+    ngOnInit() {
+        this.tasks$ = this.taskService.getAll();
+    }
 
-  onUpdate(task: Task): void {
-    this.navController.navigateForward(['tasks', 'edit', task.id]);
-  }
+    onUpdate(task: Task): void {
+        this.navController.navigateForward(['tasks', 'edit', task.id]);
+    }
+
+    async onDelete(task: Task): Promise<void> {
+        await this.overlayService.alert({
+           message: `Do you really want to delete the task "${task.title}"?`,
+            buttons: [
+                {
+                  text: 'Yes',
+                  handler: async () => {
+                      await this.taskService.delete(task);
+                      await this.overlayService.toast({
+                          message: `Task "${task.title}" deleted!`
+                      })
+                  }
+                },
+                'No'
+            ]
+        });
+    }
 
 }
